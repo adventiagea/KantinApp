7d41d866a07a5542492accf474fe5d8580026251
adventiagea <adventiahaganigea@gmail.com>
2022-02-08 13:13:53 +0700

    Membuat kelas activity

---
+++ KantinApp/app/src/main/java/com/dicoding/picodiploma/kantinapp/ListPelangganActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.kantinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPelangganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_pelanggan)
+    }
+}
