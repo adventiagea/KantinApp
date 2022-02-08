31943999dea82f0b02a9c547bb4e217adf16102c
adventiagea <adventiahaganigea@gmail.com>
2022-02-08 18:30:49 +0700

    Config UI

--- KantinApp/app/src/main/java/com/dicoding/picodiploma/kantinapp/LoginActivity.kt
+++ KantinApp/app/src/main/java/com/dicoding/picodiploma/kantinapp/LoginActivity.kt
@@ -1,11 +1,35 @@
 package com.dicoding.picodiploma.kantinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.picodiploma.kantinapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginBinding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+
+        loginBinding.loginButton.setOnClickListener {
+            val username = loginBinding.usernameInput.text.toString()
+            val password = loginBinding.passwordInput.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()){
+                val intent = Intent(this, ListPelangganActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Silahkan masukkan username dan password!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        loginBinding.registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- KantinApp/app/src/main/java/com/dicoding/picodiploma/kantinapp/RegisterActivity.kt
+++ KantinApp/app/src/main/java/com/dicoding/picodiploma/kantinapp/RegisterActivity.kt
@@ -1,11 +1,33 @@
 package com.dicoding.picodiploma.kantinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dicoding.picodiploma.kantinapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var registerBinding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+
+        registerBinding.registerButton.setOnClickListener {
+            val username = registerBinding.usernameInput.text.toString()
+            val password = registerBinding.passwordInput.text.toString()
+            val namaKantin = registerBinding.namaKantinInput.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty() && namaKantin.isNotEmpty()){
+                Toast.makeText(this, "Sukses membuat akun!", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Silahkan masukkan data yang diperlukan!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
